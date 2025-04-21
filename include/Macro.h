14a7e9dc26535f4f04a3906dd9a5fb86f575d6fc
kubikulek231 <kuba.lepik@seznam.cz>
2025-04-21 16:45:09 +0200

    Removed all files.

--- include/Macro.h
+++
@@ -1,9 +0,0 @@
-#include <cassert>
-#include <iostream>
-
-// Custom assert with message macro
-#define ASSERT_MSG(expr, msg) \
-    if (!(expr)) { \
-        std::cerr << "Assertion failed: " << #expr << "\nMessage: " << msg << "\n"; \
-        assert(expr); \
-    }
--- readme.md
+++
@@ -1,2 +0,0 @@
-## Created during the MPC-MPG course at FEEC BUT. 
-## Simple 3D PacMan Game Project
